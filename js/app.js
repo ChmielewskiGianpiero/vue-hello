c4ed499b6e35b74568bec22accbb01838cc11a88
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-23 17:17:47 +0200

    ADD - input, p and classes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
-const { createApp } = Vue;
-
+const  createApp = Vue.createApp;
 createApp({
     data() {
       return {
         title: "Vue Hello",
+        img: "img/pngwing.com.png",
+        text: "ciao ciao",
+        message: "hello vue",
+        classes: "d-flex",
       }
     }
   }).mount('#app')
